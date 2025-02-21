e2cb45aa4cd2816ef85d8f20dcd2cf24ecf7076d
Ting-Ting Chen <hchunyu.tty@gmail.com>
2025-02-21 17:02:23 +0000

    Uncomment more code for Vercel

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
     size: req.file.size,
   });
 });
-const port = process.env.PORT || 3000;
+// const port = process.env.PORT || 3000;
 // app.listen(port, function () {
 //   console.log("Your app is listening on port " + port);
 // });
